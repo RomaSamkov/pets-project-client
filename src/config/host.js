511598aad3e0e0726c705d755e20c28bf8803ff5
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-24 10:04:35 +0300

    Update host.js

--- src/config/host.js
+++ src/config/host.js
@@ -1,3 +1,3 @@
-// const HOST = 'https://pets-house-server-lg68.onrender.com';
-export const HOST = 'http://localhost:8888';
+const HOST = 'https://pets-house-server-lg68.onrender.com';
+// export const HOST = 'http://localhost:8888';
 export default HOST;
